551b5db9d2c0a3983c7dcbed37936679b0e369e0
amitpeled92 <62392209+amitpeled92@users.noreply.github.com>
2020-11-03 15:46:20 +0200

    creating ass1 project - SPL

---
+++ Assignment1SPL/cmake-build-debug/CMakeFiles/Assignment1SPL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Assignment1SPL.exe"
+  "Assignment1SPL.exe.manifest"
+  "Assignment1SPL.lib"
+  "Assignment1SPL.pdb"
+  "CMakeFiles/Assignment1SPL.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment1SPL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
